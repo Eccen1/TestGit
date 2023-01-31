4a4a7e6fb68182617c861e8f8e38ad7f8414f752
Anton Zaynutdinov <zaunytdinov@mail.ru>
2023-01-31 17:35:43 +0300

    GIT_20

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("GIT_1");
         System.out.println("GIT_2");
+        System.out.println("GIT_20");
     }
 }
